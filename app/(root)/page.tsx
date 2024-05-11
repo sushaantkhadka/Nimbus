acae8721e98d1a33a16906b839a99ce9c600533e
Sushant <sushantkhadka2060@gmail.com>
2024-05-11 20:01:37 +0545

    page change

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -5,10 +5,7 @@ import { Dialog } from "@/components/ui/dialog";
 export default function Home() {
   return (
     <div className="p-4">
-      <p>Hello world</p>
-
-      <Button>Hello</Button>
-      <Dialog>Hello world</Dialog>
+      
 
     </div>
   );
