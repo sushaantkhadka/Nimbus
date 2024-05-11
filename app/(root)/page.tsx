99a40d4e8a63346b0232c0507da7d47a6bca6dca
Sushant <sushantkhadka2060@gmail.com>
2024-05-11 19:53:27 +0545

    new component

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { Dialog } from "@/components/ui/dialog";
+
 
 export default function Home() {
   return (
     <div className="p-4">
       <p>Hello world</p>
+
+
+      <Button>Hello</Button>
+      <Dialog>Hello world</Dialog>
+
     </div>
   );
 }
