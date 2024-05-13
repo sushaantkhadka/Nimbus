0edc5814bc89adaf624a92ee8f188f6853420e79
Sushant <sushantkhadka2060@gmail.com>
2024-05-13 13:24:55 +0545

    login all set

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,11 +1,16 @@
 import { Button } from "@/components/ui/button";
+import { UserButton } from "@clerk/nextjs";
 
-export default function Home() {
+const SetupPage = () => {
   return (
     <div className="p-4">
       <p>hello</p>
-      <Button >Click ME</Button>
+      <UserButton afterSignOutUrl="/" />
+      <Button >
+        <a href="/sign-in">Login</a>
+        </Button>
 
     </div>
   );
 }
+export default SetupPage
